b4fe2f7304455faefd9689947cbab59d5158ff53
GagikManasyan <gagik_manasyan@edu.aua.am>
2021-04-27 04:33:13 +0400

    game board update, player movement update, turn update

--- Project_Monopoly/src/com/company/Game.java
+++ Project_Monopoly/src/com/company/Game.java
@@ -7,46 +7,76 @@ public class Game
     private Player player2;
     private Player player3;
     private Player player4;
-    private String [] [] game_board = new String[11][11];
-    private String [] top_line = new String[]{"|Parking|", "|Kentucky Avenue|", "|Chance|", "|Indiana Avenue|", "|Illinois Avenue|", "|Train 3|", "|Atlantic Avenue|", "|Ventnor Avenue|", "|Water|","|Marvin Gardens|", "|Go To Jail|"};
-    private String [] left_line = new String[]{"", "|New York Avenue|", "|Chest|", "|Tennessee Avenue|", "|St James Place|", "|Train 2|", "|Virginia Avenue|", "|States Avenue|", "|Electric|", "|St Charles Place|"};
-    private String [] right_line = new String[]{"", "|Pacific Avenue|", "|North Carolina Avenue|", "|Chest|", "|Pennsylvania Avenue|", "|Train 4|", "|Chance|", "|Park Place|", "|Luxury|", "|Boardwalk|"};
-    private String [] bot_line = new String []{"|Jail|", "|Connecticut Avenue|", "|Vermont Avenue|", "|Chance|", "|Oriental Avenue|", "|Train 1|", "|Tax|", "|Baltic Avenue|", "|Chest|", "|Mediterranean Avenue|", "|Go|"};
+    private static int turn = 0;
+    private String [] game_board = {"|Go|", "|Mediterranean Avenue|", "|Chest|" ,"|Baltic Avenue|", "|Tax|", "|Train 1|",  "|Oriental Avenue|", "|Chance|", "|Vermont Avenue|", "|Connecticut Avenue|","|Jail|","|St Charles Place|","|Electric|","|States Avenue|","|Virginia Avenue|","|Train 2|","|St James Place|","|Tennessee Avenue|","|Chest|","|New York Avenue|","|Parking|", "|Kentucky Avenue|", "|Chance|", "|Indiana Avenue|", "|Illinois Avenue|", "|Train 3|", "|Atlantic Avenue|", "|Ventnor Avenue|", "|Water|","|Marvin Gardens|", "|Go To Jail|","|Pacific Avenue|", "|North Carolina Avenue|", "|Chest|", "|Pennsylvania Avenue|", "|Train 4|", "|Chance|", "|Park Place|", "|Luxury|", "|Boardwalk|"};
+    private static int player_1_pos = 0;
+    private static int player_2_pos = 0;
+    private static int player_3_pos = 0;
+    private static int player_4_pos = 0;
     protected void Print_board ()
     {
         for (int i = 0; i < game_board.length; i++)
         {
-            game_board[0][i] = top_line[i];
-        }
-        for (int i = 1; i <= 9; i++)
-        {
-            game_board[i][0] = left_line[i];
-        }
-        for (int i = 1; i <= 9 ; i++)
-        {
-            game_board[i][10] = right_line[i];
-        }
-        for (int i = 0; i < bot_line.length; i++)
-        {
-            game_board[10][i] = bot_line[i];
-        }
-        for (int i = 0; i < game_board.length; i++)
-        {
-            System.out.println();
-            for (int j = 0; j < game_board.length; j++)
-            {
-                System.out.print(game_board[i][j]);
-
-            }
+            System.out.println(game_board[i]);
 
         }
     }
-    protected int Dice_roll()
+    private int diceRoll()
     {
         int dice_roll = ThreadLocalRandom.current().nextInt(1, 6 + 1) + ThreadLocalRandom.current().nextInt(1, 6 + 1);
         return dice_roll;
     }
-    protected void Feedback(){}
+    private int takeTurns()
+    {
+        turn = turn + 1;
+        if(turn > 4)
+        {
+            turn = 0;
+        }
+        return turn;
+    }
+    protected void Move()
+    {
+        int dice_roll = diceRoll();
+        int turn = takeTurns();
+        if(turn == 1)
+        {
+            this.player_1_pos += dice_roll;
+        }
+        if(turn == 2)
+        {
+            this.player_2_pos += dice_roll;
+        }
+        if(turn == 3)
+        {
+            this.player_3_pos += dice_roll;
+        }
+        if(turn == 4)
+        {
+            this.player_4_pos += dice_roll;
+        }
+    }
+    protected void Feedback()
+    {
+        if(turn == 1)
+        {
+            System.out.println("player 1 you are on" + game_board[this.player_1_pos]);
+        }
+        if(turn == 2)
+        {
+            System.out.println("player 2 you are on" + game_board[this.player_2_pos]);
+        }
+        if(turn == 3)
+        {
+            System.out.println("player 3 you are on" + game_board[this.player_3_pos]);
+        }
+        if(turn == 4)
+        {
+            System.out.println("player 4 you are on" + game_board[this.player_4_pos]);
+        }
+
+
+    }
 
 
 }
--- Project_Monopoly/src/com/company/Main.java
+++ Project_Monopoly/src/com/company/Main.java
@@ -5,11 +5,7 @@ public class Main {
     public static void main(String[] args)
     {
         Game game = new Game();
-        game.Print_board();
-        /*hello*/
-        /*bye*/
-        /*change*/
-        /*hello*/
-        /*test*/
+
+
     }
 }
