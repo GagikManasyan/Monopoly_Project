6b7a6f4f7aea66261c8828210feefbf6505db77d
Azat <hovakimyan.azat@gmail.com>
2021-04-28 23:15:37 +0400

    Test

--- Project_Monopoly/src/com/company/Game.java
+++ Project_Monopoly/src/com/company/Game.java
@@ -8,6 +8,52 @@ public class Game
     private Player player3;
     private Player player4;
     private static int turn = 0;
+    Square [] game_board1;
+    Game (String player1, String player2, String player3, String player4){
+        game_board1[0] = new Square("Go");
+        game_board1[1] = new Street("Mediterranean Avenue");
+        game_board1[2] = new Community_chest();
+        game_board1[3] = new Street("Baltic Avenue");
+        game_board1[4] = new Square("Tax");
+        game_board1[5] = new Train("Train 1");
+        game_board1[6] = new Street("Oriental Avenue");
+        game_board1[7] = new Community_chest();
+        game_board1[8] = new Street("Vermont Avenue");
+        game_board1[9] = new Street("Connecticut Avenue");
+        /*game_board1[10] = new Jail("Jail");*/
+        game_board1[11] =
+        game_board1[12]
+        game_board1[13]
+        game_board1[14]
+        game_board1[15]
+        game_board1[16]
+        game_board1[17]
+        game_board1[18]
+        game_board1[19]
+        game_board1[20]
+        game_board1[21]
+        game_board1[22]
+        game_board1[23]
+        game_board1[24]
+        game_board1[25]
+        game_board1[26]
+        game_board1[27]
+        game_board1[28]
+        game_board1[29]
+        game_board1[30]
+        game_board1[31]
+        game_board1[32]
+        game_board1[33]
+        game_board1[34]
+        game_board1[35]
+        game_board1[36]
+        game_board1[37]
+        game_board1[38]
+        game_board1[39]
+
+
+
+    }
     private String [] game_board =
             {"|Go|",
             "|Mediterranean Avenue|",
